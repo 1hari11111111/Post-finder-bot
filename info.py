42d9ae4194cae860d0fffaaa3c440009d25721d4
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-03-04 19:47:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
+API_ID       = 26042863
+API_HASH     = "d4fabc00b0345cd3f0ccdc0c9b750f6e"
 BOT_TOKEN    = ""
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  =
+DATABASE_URI = "mongodb+srv://AutoFiltersBot:SGBACKUP@cluster0.sko0xmx.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = "-1001994332079"
 ADMIN        = 919169586
 CHANNEL      = ""
