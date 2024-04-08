4f41fce176ba054d4f20eceb2d62da4b159811b6
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-04-08 12:38:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = ""
-ADMIN        = 
-CHANNEL      = ""
+API_ID       = 26042863
+API_HASH     = "d4fabc00b0345cd3f0ccdc0c9b750f6e"
+BOT_TOKEN    = "6934545157:AAGHlmQX9GcfFj02Msm72KFli9gmXODSE6w"
+SESSION      = "BQGNYe8ANS2l3O6cIhcpZYup2ypx5K0hqK5hdrolRIvduKilbeHgSxaWpiMDluHUv5B28bfS_YsouMuzyl5d0QtkjD7XbamiB6i2oUnH9ftMMW3yVHG-RdKqVQe_V62EXp2vra0CzD4Vs7KWs4e0nA3s0_hUzcYrjjqnNUTDoz4ajswZbSTmXeI-qLWH1YH1c8qvYINvv5HoSmZQRGch5G8hBQWtnenCss3xaCRjkI389OtBKKnCOPXn2UYLnnYZhQI_1HRPwnAFMXKKINWcP60whiZkmFgrRV67kQZ76i6Xzf-dPU0g1EM6tnyQAMWrqsyn5pvlzfzL-jDt7Z0ZU9uJ_W-djAAAAAFwASoqAA"
+DATABASE_URI = "mongodb+srv://subhajitghosh7777777:ABJtWJizF74zkVZg@cluster0.3v5w06k.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = "-1001994332079"
+ADMIN        = 919169586
+CHANNEL      = "-1001877309572"
