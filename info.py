f9034dc6309b6a27f1eca8a8bc81e324ce71fd90
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-04-11 17:20:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 26042863
 API_HASH     = "d4fabc00b0345cd3f0ccdc0c9b750f6e"
 BOT_TOKEN    = "6934545157:AAGHlmQX9GcfFj02Msm72KFli9gmXODSE6w"
 SESSION      = "BQHFENsAVqvrzPzkcCjpj0QgzqDK359NDsaw3DnEhPpTF0txLuXDNJutnYV85XtKMG-OROUnxsY7eNsrjyrOcsH0UBfYh85YquqmkudkH7xxbrAiEU0GlqQzq-NUQ2HYB2x2d-9wvSsbVZgS9M_bBC8tNyXpTmTQ67U5N24hD0J-BhOAmNaj9U6OQKl9EwE80cNbsqVw-Inz5SNlRH4j352WrEq8flrFyZdmL7-pSBzaMijoFbOP-FaogmQ3yDBNxXiTyC0rPn19ufohnBtS6-0CeNhf4gqSJME1oOocfCPIzUmOpWv0L0iGgWHM9AvpwcXOnJowsGVVIS8XiuFIHkg1tFZezQAAAAFwASoqAA"
-DATABASE_URI = "mongodb+srv://subhajitghosh7777777:ABJtWJizF74zkVZg@cluster0.3v5w06k.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://SGBACKUP:SGBACKUP@cluster0.ofwdtva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = "-1001994332079"
 ADMIN        = 919169586
 CHANNEL      = ""
