93024123e45547dae2e17c973e8d17024769903a
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-07-10 07:09:47 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,84 +1,5 @@
 import asyncio 
 from info import *
-from pyrogrimport asyncio 
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from pyrogram.errors import UserNotParticipant
-from pyrogram.types import ChatPermissions
-from pymongo.errors import DuplicateKeyError
-from motor.motor_asyncio import AsyncIOMotorClient
-from imdb import Cinemagoer
-from pyrogram import enums
-
-# Initialize MongoDB client and collections
-DATABASE_URI = "your_database_uri"
-dbclient = AsyncIOMotorClient(DATABASE_URI)
-db = dbclient["Channel-Filter"]
-grp_col = db["GROUPS"]
-user_col = db["USERS"]
-dlt_col = db["Auto-Delete"]
-
-ia = Cinemagoer()
-
-# Function to add a group to MongoDB
-async def add_group(group_id, group_name, user_name, user_id, channels, f_sub, verified):
-    data = {
-        "_id": group_id,
-        "name": group_name, 
-        "user_id": user_id,
-        "user_name": user_name,
-        "channels": channels,
-        "f_sub": f_sub,
-        "verified": verified
-    }
-    try:
-       await grp_col.insert_one(data)
-    except DuplicateKeyError:
-       pass
-
-# Function to get a group from MongoDB
-async def get_group(id):
-    data = {'_id': id}
-    group = await grp_col.find_one(data)
-    if group is None:
-        return {}
-    return dict(group)
-
-# Function to update group data in MongoDB
-async def update_group(id, new_data):
-    data = {"_id": id}
-    new_value = {"$set": new_data}
-    await grp_col.update_one(data, new_value)
-
-# Function to delete a group from MongoDB
-async def delete_group(id):
-    data = {"_id": id}
-    await grp_col.delete_one(data)
-
-# Function to get all groups from MongoDB
-async def get_groups():
-    count = await grp_col.count_documents({})
-    cursor = grp_col.find({})
-    list = await cursor.to_list(length=int(count))
-    return count, list
-
-# Function to add a user to MongoDB
-async def add_user(id, name):
-    data = {"_id": id, "name": name}
-    try:
-       await user_col.insert_one(data)
-    except DuplicateKeyError:
-       pass
-
-# Function to get all users from MongoDB
-async def get_users():
-    count = await user_col.count_documents({})
-import asyncio 
-from info import *
-from pyrogram import enums
-from imdb import Cinemagoer
-import asyncio 
-from info import *
 from pyrogram import enums
 from imdb import Cinemagoer
 from pymongo.errors import DuplicateKeyError
@@ -198,4 +119,4 @@ async def force_sub(bot, message):
        await bot.send_message(chat_id=admin, text=f"❌ Error in Fsub:\n`{str(e)}`")
        return False 
     else:
-       return True
+       return True 
